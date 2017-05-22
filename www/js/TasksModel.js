b485e797ee455567a01e9e055a0fc4921090cc24
Vitor de Souza <vitordesolza@gmail.com>
2017-05-22 11:29:20 -0300

    Código para FORTEI - V2

---
+++ www/js/TasksModel.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Vídeo 05
+function getTasks() {
+    this.items = [
+        {nome: "Item 1", finalizada: false},
+        {nome: "Item 2", finalizada: false},
+        {nome: "Item 3", finalizada: false},
+        {nome: "Item 4", finalizada: false}
+    ];
+}
+
--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +21,10 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.controller('mainController', function($scope){
+    var tasks = new getTasks();
+    $scope.items = tasks.items;
+  
+});
